08d072b72e512ff7592d669e560da8e56dfca3d6
Allan Danos <allan.danos@gmail.com>
2014-05-25 02:08:45 +0800

    Added files to ignore

--- platforms/android/ant-gen/R.java.d
+++
@@ -1,9 +0,0 @@
-/Users/allan/workspace/cordova/p2p/platforms/android/ant-gen/com/lib/p2p/R.java \
- : /Users/allan/workspace/cordova/p2p/platforms/android/res/drawable/icon.png \
-/Users/allan/workspace/cordova/p2p/platforms/android/res/drawable-hdpi/icon.png \
-/Users/allan/workspace/cordova/p2p/platforms/android/res/drawable-ldpi/icon.png \
-/Users/allan/workspace/cordova/p2p/platforms/android/res/drawable-mdpi/icon.png \
-/Users/allan/workspace/cordova/p2p/platforms/android/res/drawable-xhdpi/icon.png \
-/Users/allan/workspace/cordova/p2p/platforms/android/res/values/strings.xml \
-/Users/allan/workspace/cordova/p2p/platforms/android/res/xml/config.xml \
-/Users/allan/workspace/cordova/p2p/platforms/android/ant-build/AndroidManifest.xml \
